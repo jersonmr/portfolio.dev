4c56180fdf7a74665866b199420a0b7ab9300065
Jerson Moreno <jersonmr@gmail.com>
2024-01-25 23:25:14 -0400

    Remove console logs

--- src/scripts/formatDate.js
+++ src/scripts/formatDate.js
@@ -1,6 +1,4 @@
 export function formatDate(dateString) {
-    console.log(dateString);
-    // return new Date(`${dateString}T00:00:00Z`).toLocaleDateString("en-US", {
     return dateString.toLocaleDateString("es-VE", {
         day: "numeric",
         month: "long",
